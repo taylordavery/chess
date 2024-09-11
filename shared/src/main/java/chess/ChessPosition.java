57835e94ec6de5e4b598a6c069994a60bd6fc943
Taylor Avery <taylordavery@gmail.com>
2024-09-10 21:14:01 -0600

    Allow bishop move test to run

---
+++ shared/src/main/java/chess/ChessPosition.java
@@ -0,0 +1,34 @@
+package chess;
+
+/**
+ * Represents a single square position on a chess board
+ * <p>
+ * Note: You can add to this class, but you may not alter
+ * signature of the existing methods.
+ */
+public class ChessPosition {
+
+    private final int row;
+    private final int col;
+
+    public ChessPosition(int row, int col) {
+        this.row = row;
+        this.col = row;
+    }
+
+    /**
+     * @return which row this position is in
+     * 1 codes for the bottom row
+     */
+    public int getRow() {
+        return row;
+    }
+
+    /**
+     * @return which column this position is in
+     * 1 codes for the left row
+     */
+    public int getColumn() {
+        return col;
+    }
+}
