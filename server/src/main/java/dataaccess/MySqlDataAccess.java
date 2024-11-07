e803f4cc23d29a7d64229ba8c689ff44d1702e0e
Taylor Avery <taylordavery@gmail.com>
2024-11-06 22:40:45 -0700

    Make some more changes 11/12

--- server/src/main/java/dataaccess/MySqlDataAccess.java
+++ server/src/main/java/dataaccess/MySqlDataAccess.java
@@ -265,6 +265,7 @@ public class MySqlDataAccess implements DataAccess {
             ps.setString(4, json);
             ps.executeUpdate();
 
+
             try (var rs = ps.getGeneratedKeys()) {
                 if (rs.next()) {
                     int gameID = rs.getInt(1);
