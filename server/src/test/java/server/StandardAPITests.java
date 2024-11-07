9bdb899fab47f6fe4527fab1e05f319956cb71ab
Taylor Avery <taylordavery@gmail.com>
2024-11-06 22:45:35 -0700

    Make some more changes

--- server/src/test/java/server/StandardAPITests.java
+++ server/src/test/java/server/StandardAPITests.java
@@ -33,18 +33,24 @@ public class StandardAPITests {
 
     @BeforeAll
     public static void init() throws DataAccessException {
-        server = new Server();
-        var port = server.run(0);
-        System.out.println("Started test HTTP server on " + port);
+        try {
+            server = new Server();
+            var port = server.run(0);
+            System.out.println("Started test HTTP server on " + port);
 
-        serverFacade = new TestServerFacade("localhost", Integer.toString(port));
+            serverFacade = new TestServerFacade("localhost", Integer.toString(port));
 
-        existingUser = new TestUser("ExistingUser", "existingUserPassword", "eu@mail.com");
+            existingUser = new TestUser("ExistingUser", "existingUserPassword", "eu@mail.com");
 
-        newUser = new TestUser("NewUser", "newUserPassword", "nu@mail.com");
+            newUser = new TestUser("NewUser", "newUserPassword", "nu@mail.com");
 
-        createRequest = new TestCreateRequest("testGame");
+            createRequest = new TestCreateRequest("testGame");
+        } catch (DataAccessException e) {
+            throw new DataAccessException(e.getMessage());
+            // Handle the exception or throw a runtime exception if necessary
+        }
     }
+
 
     @BeforeEach
     public void setup() {
