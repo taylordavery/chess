c1942fbbf3894617715111efbe15be8109c5ae56
Taylor Avery <taylordavery@gmail.com>
2024-11-06 22:56:44 -0700

    Make some more changes

--- server/src/test/java/passoff/server/StandardAPITests.java
+++ server/src/test/java/passoff/server/StandardAPITests.java
@@ -1,10 +1,11 @@
-package server;
+package passoff.server;
 
 import chess.ChessGame;
 import dataaccess.DataAccessException;
 import org.junit.jupiter.api.*;
 import passoff.model.*;
 import passoff.server.TestServerFacade;
+import server.Server;
 
 import java.net.HttpURLConnection;
 import java.util.Arrays;
