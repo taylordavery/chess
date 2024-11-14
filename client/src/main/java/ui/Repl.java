9166b184e7120f822d170a2be02a6c0db520ba22
Taylor Avery <taylordavery@gmail.com>
2024-11-13 22:38:47 -0700

    Start implementing PreLoginClient

---
+++ client/src/main/java/ui/Repl.java
@@ -0,0 +1,50 @@
+package ui;
+
+//import client.websocket.NotificationHandler;
+//import webSocketMessages.Notification;
+
+import java.util.Scanner;
+
+import static com.sun.org.apache.xalan.internal.xsltc.compiler.Constants.RESET;
+import static java.awt.Color.*;
+import static ui.EscapeSequences.*;
+
+//public class Repl implements NotificationHandler {
+public class Repl {
+    private final PreLoginClient client;
+
+    public Repl(String serverUrl) {
+        client = new PreLoginClient(serverUrl, this);
+    }
+
+    public void run() {
+        System.out.println("\uD83D\uDC36 Welcome to the Chess. Sign in to start.");
+        System.out.print(client.help());
+
+        Scanner scanner = new Scanner(System.in);
+        var result = "";
+        while (!result.equals("quit")) {
+            printPrompt();
+            String line = scanner.nextLine();
+
+            try {
+                result = client.eval(line);
+                System.out.print(BLUE + result);
+            } catch (Throwable e) {
+                var msg = e.toString();
+                System.out.print(msg);
+            }
+        }
+        System.out.println();
+    }
+
+//    public void notify(Notification notification) {
+//        System.out.println(RED + notification.message());
+//        printPrompt();
+//    }
+
+    private void printPrompt() {
+        System.out.print("\n" + RESET + ">>> " + GREEN);
+    }
+
+}
